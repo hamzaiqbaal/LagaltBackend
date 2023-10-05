3022afb688dba2f60c7b6d51e16823cc64c864f8
Hamza Iqbal <hamzaiqbal0512@gmail.com>
2023-10-05 09:43:40 +0200

    initial commit

---
+++ src/main/java/com/example/lagaltcaseapplication/LagaltCaseApplication.java
@@ -0,0 +1,13 @@
+package com.example.lagaltcaseapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LagaltCaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LagaltCaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lagaltcaseapplication/LagaltCaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lagaltcaseapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LagaltCaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
