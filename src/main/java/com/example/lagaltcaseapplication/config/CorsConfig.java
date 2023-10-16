b1063c94246375af71588a39e64e6d45ae3accb1
Hamza Iqbal <hamzaiqbal0512@gmail.com>
2023-10-16 11:23:39 +0200

    ConfigCORS

---
+++ src/main/java/com/example/lagaltcaseapplication/config/CorsConfig.java
@@ -0,0 +1,15 @@
+package com.example.lagaltcaseapplication.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost:3000") // specify your frontend origin here
+                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedHeaders("Content-Type", "Authorization");
+    }
+}
