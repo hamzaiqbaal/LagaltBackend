39bff76f3baacec951f7b154af96a313aa0328cf
Hamza Iqbal <hamzaiqbal0512@gmail.com>
2023-10-19 01:22:51 +0200

    creating participants application etc

--- src/main/java/com/example/lagaltcaseapplication/mapper/WorkApplicationMapper.java
+++ src/main/java/com/example/lagaltcaseapplication/mapper/WorkApplicationMapper.java
@@ -2,21 +2,56 @@ package com.example.lagaltcaseapplication.mapper;
 
 import com.example.lagaltcaseapplication.dto.CommentDTO;
 import com.example.lagaltcaseapplication.dto.WorkApplicationDTO;
+import com.example.lagaltcaseapplication.exceptions.ProjectNotFoundException;
+import com.example.lagaltcaseapplication.exceptions.UserNotFoundException;
 import com.example.lagaltcaseapplication.models.Comment;
+import com.example.lagaltcaseapplication.models.Project;
+import com.example.lagaltcaseapplication.models.User;
 import com.example.lagaltcaseapplication.models.WorkApplication;
+import com.example.lagaltcaseapplication.repository.ProjectRepository;
+import com.example.lagaltcaseapplication.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class WorkApplicationMapper {
 
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private ProjectRepository projectRepository;
+
+    @Autowired
+    private UserMapper userMapper;
+
     public WorkApplication toEntity(WorkApplicationDTO workApplicationDTO) {
         WorkApplication workApplication = new WorkApplication();
+
+        workApplication.setApplication_id(workApplicationDTO.getApplicationId());
+
+        // Fetch Project and User entities from their repositories
+        Project project = projectRepository.findById(workApplicationDTO.getProjectId())
+                .orElseThrow(() -> new ProjectNotFoundException(workApplicationDTO.getProjectId()));
+        User user = userRepository.findById(workApplicationDTO.getUserId())
+                .orElseThrow(() -> new UserNotFoundException(workApplicationDTO.getUserId()));
+
+        workApplication.setProject(project);
+        workApplication.setUser(user);
+        workApplication.setAccepted(workApplicationDTO.isAccepted());
+        workApplication.setMotivation(workApplicationDTO.getMotivation());
 
         return workApplication;
     }
 
     public WorkApplicationDTO toDTO(WorkApplication workApplication) {
         WorkApplicationDTO workApplicationDTO = new WorkApplicationDTO();
+
+        workApplicationDTO.setApplicationId(workApplication.getApplication_id());
+        workApplicationDTO.setProjectId(workApplication.getProject().getProjectId());
+        workApplicationDTO.setUserId(workApplication.getUser().getUserId());
+        workApplicationDTO.setAccepted(workApplication.isAccepted());
+        workApplicationDTO.setMotivation(workApplication.getMotivation());
 
         return workApplicationDTO;
     }
--- src/main/java/com/example/lagaltcaseapplication/models/WorkApplication.java
+++ src/main/java/com/example/lagaltcaseapplication/models/WorkApplication.java
@@ -1,12 +1,19 @@
 package com.example.lagaltcaseapplication.models;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@Table(name = "work_application")
 public class WorkApplication {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "application_id")
     private Long application_id;
 
     @ManyToOne
@@ -17,8 +24,10 @@ public class WorkApplication {
     @JoinColumn(name = "user_id", referencedColumnName = "user_id")
     private User user;
 
+    @Column(name = "is_accepted")
     private boolean isAccepted;
 
+    @Column(name = "motivation")
     private String motivation;
 
 }
