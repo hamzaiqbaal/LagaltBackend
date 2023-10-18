d02422efc10420ad63c6128a5d57a8704171f263
Hamza Iqbal <hamzaiqbal0512@gmail.com>
2023-10-18 12:24:17 +0200

    creating workapplication

---
+++ src/main/java/com/example/lagaltcaseapplication/repository/WorkApplicationRepository.java
@@ -0,0 +1,7 @@
+package com.example.lagaltcaseapplication.repository;
+import com.example.lagaltcaseapplication.models.WorkApplication;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface WorkApplicationRepository extends JpaRepository<WorkApplication, Long> {
+
+}
