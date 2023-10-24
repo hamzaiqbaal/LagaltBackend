5646543299fc758a9afdefbf5c0ba7b31c000218
BTesta <humbabast@hotmail.fr>
2023-10-24 08:48:08 +0200

    some changes for backend keycloak, Having some bug and missing cobling user from db and jwt sub

---
+++ src/main/java/com/example/lagaltcaseapplication/config/SecurityConfig.java
@@ -0,0 +1,43 @@
+package com.example.lagaltcaseapplication.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationConverter;
+import org.springframework.security.oauth2.server.resource.authentication.JwtGrantedAuthoritiesConverter;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.cors().and().sessionManagement().disable()
+                .csrf().disable()
+                .authorizeHttpRequests(authorize -> authorize
+                        // Define the mappings
+                        // projects/ 200
+
+                        .antMatchers("/projects/", "/swagger-ui.html", "/v2/api-docs", "/swagger-resources/**", "/webjars/**").permitAll()
+                        // api/v1/resources/authorized 403
+                        .antMatchers("/projects/id").hasRole("USER")
+                        .anyRequest().authenticated()
+                )
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(jwtAuthenticationConverter());
+        return http.build();
+    }
+
+    @Bean
+    public JwtAuthenticationConverter jwtAuthenticationConverter() {
+        JwtGrantedAuthoritiesConverter grantedAuthoritiesConverter = new JwtGrantedAuthoritiesConverter();
+        grantedAuthoritiesConverter.setAuthoritiesClaimName("roles");
+        grantedAuthoritiesConverter.setAuthorityPrefix("ROLE_");
+
+        JwtAuthenticationConverter jwtAuthenticationConverter = new JwtAuthenticationConverter();
+        jwtAuthenticationConverter.setJwtGrantedAuthoritiesConverter(grantedAuthoritiesConverter);
+        return jwtAuthenticationConverter;
+    }
+}
